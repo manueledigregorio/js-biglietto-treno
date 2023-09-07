814dad071b095eb8b67e574b7374c6a5dfc3530e
manueledigregorio <dig.manuele@gmail.com>
2023-09-07 16:28:46 +0200

    [add] condizione

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-const kilometriPercorsi =('quanti kilometri vuoi fare ?')
-const età = ('Età del passeggero')
+const kilometriPercorsi = parseInt(prompt('quanti kilometri vuoi fare ?'));
+const età = parseInt(prompt('Età del passeggero'));
+const costoBiglietto = kilometriPercorsi * 0.21 ;
+
+const minorenne = 18;
+const over = 65;
+
+if((minorenne ))
+
