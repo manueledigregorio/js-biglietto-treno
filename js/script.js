7dff6b91a80353e4e0b9e89fe27dfd9ecd1d934d
manueledigregorio <dig.manuele@gmail.com>
2023-09-08 00:52:12 +0200

    [FIX] percentuale sconto , messaggio

--- js/script.js
+++ js/script.js
@@ -2,31 +2,26 @@ const kilometriPercorsi = parseFloat(prompt('quanti kilometri vuoi fare ?'));
 const age = parseInt(prompt('Età del passeggero'));
 const costoBiglietto = kilometriPercorsi * 0.21  ;
 
+
+let percentualesconto = 0;
 const minorenne = 18;
 const over = 65;
-let sconto;
+let sconto = 0;
 let messaggio;
 
 if((age < minorenne)){
-  sconto = costoBiglietto * 20 / 100;
 
-  messaggio = (costoBiglietto - sconto).toFixed(2) + " €";
-
+  percentualesconto = 20;
 }
 
  else if(age > over ){
 
-  sconto = costoBiglietto * 40 / 100;
-
-  messaggio = (costoBiglietto - sconto).toFixed(2) + " €";
+ percentualesconto = 40;
 
 }
 
-else{
-  messaggio = costoBiglietto.toFixed(2) + " €";
-}
-
-
+sconto = costoBiglietto * percentualesconto / 100;
+messaggio = (costoBiglietto - sconto).toFixed(2) + " €";
 
 document.getElementById("output").innerHTML = messaggio;
 
