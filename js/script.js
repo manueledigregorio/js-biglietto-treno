a630cd2fb41982c75f719c390a6eb7b8a37fc93a
manueledigregorio <dig.manuele@gmail.com>
2023-09-08 15:43:19 +0200

    [fix] aggiunti controlli

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
-const kilometriPercorsi = parseFloat(prompt('quanti kilometri vuoi fare ?'));
-const age = parseInt(prompt('Età del passeggero'));
-const costoBiglietto = kilometriPercorsi * 0.21  ;
 
-
-let percentualesconto = 0;
+let percentualesconto;
 const minorenne = 18;
 const over = 65;
 let sconto = 0;
 let messaggio;
+let insertok = true ;
 
-if((age < minorenne)){
+const kilometriPercorsi = parseFloat(prompt('quanti kilometri vuoi fare ?'));
+const age = parseInt(prompt('Età del passeggero'));
+const costoBiglietto = kilometriPercorsi * 0.21  ;
 
-  percentualesconto = 20;
+if(isNaN(kilometriPercorsi)|| isNaN(age)){
+  insertok = false ;
+  messaggio = "Errore! dati non corretti!";
+
+   
+    //SE NON è UN NUMERO ALLORA è FALSO
 }
 
- else if(age > over ){
+if(insertok){
 
- percentualesconto = 40;
+  if((age < minorenne)){
+
+    percentualesconto = 20;
+    sconto = costoBiglietto * percentualesconto / 100;
+  }
+  
+   else if(age > over ){
+  
+   percentualesconto = 40;
+   sconto = costoBiglietto * percentualesconto / 100;
+  
+  }
+  messaggio = (costoBiglietto - sconto).toFixed(2) + " €";
 
 }
-
-sconto = costoBiglietto * percentualesconto / 100;
-messaggio = (costoBiglietto - sconto).toFixed(2) + " €";
 
 document.getElementById("output").innerHTML = messaggio;
 
